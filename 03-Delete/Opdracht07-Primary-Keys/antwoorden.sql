9bb09ba2a7bfe98ce53667cd11a9b7d2ea8d2cf9
Timothy van den Beitel <2157002@talnet.nl>
2022-02-07 13:48:52 +0100

    DELETE 7

--- 03-Delete/Opdracht07-Primary-Keys/antwoorden.sql
+++ 03-Delete/Opdracht07-Primary-Keys/antwoorden.sql
@@ -1,5 +1,15 @@
 -- Opdracht 1
+SELECT * FROM `players` WHERE id = 198329;
+
 
 -- Opdracht 2
+DELETE FROM `players` WHERE id = 198329;
 
 -- Opdracht 3
+SELECT * FROM `players` WHERE id = 199304;
+SELECT * FROM `players` WHERE id = 207494;
+SELECT * FROM `players` WHERE id = 226753;
+
+DELETE FROM `players` WHERE id = 199304;
+DELETE FROM `players` WHERE id = 207494;
+DELETE FROM `players` WHERE id = 226753;
